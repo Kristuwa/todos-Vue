6149b795a677624ec3a2578b04bfed707dad4da4
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2023-09-08 11:41:35 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/todos-vue/" : "/",
+  publicPath: "/todos-Vue/",
   transpileDependencies: true,
 });
